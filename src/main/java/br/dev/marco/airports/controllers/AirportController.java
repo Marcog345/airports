7ded6ff703e443f80a5ec7136a4343e372624e53
Marco <marcoantoniosouzasantos888@gmail.com>
2025-12-04 16:00:32 -0300

    Término da Fase 5 –Update interface AirportRepository – Adicionado assinatura de método Airport findByIataCode(). -Update classe AirportService -Adicionado método findByIataCodeSearch. -Update classe AirportController – Adicionado endpoint iatacode/{iataCode}.

--- src/main/java/br/dev/marco/airports/controllers/AirportController.java
+++ src/main/java/br/dev/marco/airports/controllers/AirportController.java
@@ -44,4 +44,14 @@ public class AirportController {
             return ResponseEntity.ok(result);
         }
     }
+    @GetMapping("/iatacode/{iataCode}")
+    public ResponseEntity<Airport> findByIataCode(@PathVariable String iataCode){
+        Airport result = airportService.findByIataCode(iataCode);
+        
+        if (result == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(result);
+        }
+    }
 }
--- src/main/java/br/dev/marco/airports/service/AirportService.java
+++ src/main/java/br/dev/marco/airports/service/AirportService.java
@@ -29,4 +29,8 @@ public class AirportService
                 .map(x -> new AirportMinDTO(x)).toList();
         return resultDTO;
     }
+    public Airport findByIataCode(String iataCode){
+        Airport result = airportRepository.findByIataCode(iataCode);
+        return result;
+    }
 }
